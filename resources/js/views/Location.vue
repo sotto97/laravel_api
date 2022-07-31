ac4f8413eabb31c18d21c30777b4102a2a06f1aa
sotto97_win <sotto.bw97@gmail.com>
2022-07-31 23:15:15 +0900

    位置情報取得のプログラム

---
+++ resources/js/views/Location.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="w-full p-2">
+        <!-- タイトル -->
+        <div class="p-2 flex">
+            <div class="w-1/2">
+                <p class="font-semibold text-xl">位置情報</p>
+            </div>
+        </div>
+        <!-- 位置情報取得ボタン -->
+        <div class="">
+            <form @submit.prevent="getLocation">
+                <button type="submit" class="py-2 px-4 w-1/6 cursor-pointer bg-zinc-800 text-white rounded-md hover:bg-zinc-500 transition">位置情報を取得する</button>
+            </form>
+        </div>
+        <!-- 一覧描画 -->
+        <table class="w-full text-center border-collapse border border-slate-500">
+            <thead class="">
+                <tr class="border border-slate-600 h-12 hover:bg-zinc-800 hover:text-white cursor-pointer transition">
+                    <th>県</th>
+                    <th>緯度</th>
+                    <th>経度</th>
+                </tr>
+            </thead>
+            <tbody class="">
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        Lat: "", // 緯度
+        Lon: "", // 経度
+        // alertSuccess: false,
+        // alertFail: false,
+    }),
+    methods: {
+        getLocation: function () {
+            // 現在地を取得
+            navigator.geolocation.getCurrentPosition(
+                // 取得成功した場合
+                function (position) {
+                    alert("緯度:" + position.coords.latitude + ",経度" + position.coords.longitude);
+                },
+                // 取得失敗した場合
+                function (error) {
+                    switch (error.code) {
+                        case 1: //PERMISSION_DENIED
+                            alert("位置情報の利用が許可されていません");
+                            break;
+                        case 2: //POSITION_UNAVAILABLE
+                            alert("現在位置が取得できませんでした");
+                            break;
+                        case 3: //TIMEOUT
+                            alert("タイムアウトになりました");
+                            break;
+                        default:
+                            alert("その他のエラー(エラーコード:" + error.code + ")");
+                            break;
+                    }
+                }
+            );
+        },
+    },
+};
+</script>
